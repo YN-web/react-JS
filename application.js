515ff4c180e217a84a140b1b5af1cc77160aa1f7
Patrick <iradukundapatrick02@gmail.com>
2025-05-08 04:39:31 -0700

    Initial Commit

---
+++ application.js
@@ -0,0 +1,13 @@
+for(let index=0; index < 10; index++) {
+    console.log("Hello World \n");
+}
+
+for (let a = 0; a < 3; a++) {
+        console.log(" # # # # # #"); 
+    console.log("# # # # # #");
+   
+}
+ console.log("\n");
+for (let index = 1; index < 5; index++){
+        console.log("*".repeat(index));   
+}
