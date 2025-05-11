47b098afd84798dc07a56789eaf85bed764f04d4
Patrick <iradukundapatrick02@gmail.com>
2025-05-11 09:35:00 +0200

    Initial Commit

--- application.js
+++ application.js
@@ -11,3 +11,26 @@ for (let a = 0; a < 3; a++) {
 for (let index = 1; index < 5; index++){
         console.log("*".repeat(index));   
 }
+
+let myNumber =(num1,num2) => num1 * num2 /2;
+     console.log(myNumber(10,20)); 
+
+ void function greetings() {
+    console.log("Hello World");
+
+}()
+
+// Objects 
+    let person= {
+        FirstName : 'Patrick',
+        LastName : 'Ouch',
+        Age : 21,
+        City : 'Kigali',
+        greetings : function () {
+            return `Hello  ${this.FirstName} ${this.LastName}`
+        }
+    }
+    console.log(person.greetings());
+ 
+
+    
